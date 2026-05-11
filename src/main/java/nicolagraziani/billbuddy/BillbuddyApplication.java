f4c2615aab6e6942ad905a9700665dac8778ae3f
Graziani Nicola <graziani.nicola2000@gmail.com>
2026-05-11 13:28:26 +0200

    Initial setup

---
+++ src/main/java/nicolagraziani/billbuddy/BillbuddyApplication.java
@@ -0,0 +1,13 @@
+package nicolagraziani.billbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nicolagraziani/billbuddy/BillbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package nicolagraziani.billbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
